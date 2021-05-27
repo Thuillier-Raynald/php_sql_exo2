0e36b39230ac279dfb17363284c77efd1df8f900
Thuillier Raynald <thuillier.raynald@gmail.com>
2021-05-27 12:38:17 +0200

    Add file .sql

---
+++ session.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : jeu. 27 mai 2021 à 10:36
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `session`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `abonne`
+--
+
+DROP TABLE IF EXISTS `abonne`;
+CREATE TABLE IF NOT EXISTS `abonne` (
+  `id_abonne` int(3) NOT NULL AUTO_INCREMENT,
+  `prenom` varchar(15) NOT NULL,
+  PRIMARY KEY (`id_abonne`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `abonne`
+--
+
+INSERT INTO `abonne` (`id_abonne`, `prenom`) VALUES
+(1, 'Guillaume'),
+(2, 'Benoit'),
+(3, 'Chloe'),
+(4, 'Laura');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `emprunt`
+--
+
+DROP TABLE IF EXISTS `emprunt`;
+CREATE TABLE IF NOT EXISTS `emprunt` (
+  `id_emprunt` int(3) NOT NULL AUTO_INCREMENT,
+  `id_livre` int(3) DEFAULT NULL,
+  `id_abonne` int(3) DEFAULT NULL,
+  `date_sortie` date NOT NULL,
+  `date_rendu` date DEFAULT NULL,
+  PRIMARY KEY (`id_emprunt`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `emprunt`
+--
+
+INSERT INTO `emprunt` (`id_emprunt`, `id_livre`, `id_abonne`, `date_sortie`, `date_rendu`) VALUES
+(1, 100, 1, '2011-12-17', '2011-12-18'),
+(2, 101, 2, '2011-12-18', '2011-12-20'),
+(3, 100, 3, '2011-12-19', '2011-12-22'),
+(4, 103, 4, '2011-12-19', '2011-12-22'),
+(5, 104, 1, '2011-12-19', '2011-12-28'),
+(6, 105, 2, '2012-03-20', '2012-03-26'),
+(7, 105, 3, '2013-06-13', NULL),
+(8, 100, 2, '2013-06-15', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `livre`
+--
+
+DROP TABLE IF EXISTS `livre`;
+CREATE TABLE IF NOT EXISTS `livre` (
+  `id_livre` int(3) NOT NULL AUTO_INCREMENT,
+  `auteur` varchar(25) NOT NULL,
+  `titre` varchar(30) NOT NULL,
+  PRIMARY KEY (`id_livre`)
+) ENGINE=InnoDB AUTO_INCREMENT=106 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `livre`
+--
+
+INSERT INTO `livre` (`id_livre`, `auteur`, `titre`) VALUES
+(100, 'GUY DE MAUPASSANT', 'Une vie'),
+(101, 'GUY DE MAUPASSANT', 'Bel-Ami '),
+(102, 'HONORE DE BALZAC', 'Le pere Goriot'),
+(103, 'ALPHONSE DAUDET', 'Le Petit chose'),
+(104, 'ALEXANDRE DUMAS', 'La Reine Margot'),
+(105, 'ALEXANDRE DUMAS', 'Les Trois Mousquetaires');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
